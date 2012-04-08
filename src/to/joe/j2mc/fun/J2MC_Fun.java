e3ccfa43b9b4fb0e6523f1389d7d120540934a6e
jamietech <jamie@jamietech.co.cc>
2012-04-08 15:41:16 +1000

    Fixing flying

--- src/to/joe/j2mc/fun/J2MC_Fun.java
+++ src/to/joe/j2mc/fun/J2MC_Fun.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import to.joe.j2mc.fun.command.ClearInventoryCommand;
@@ -48,6 +49,12 @@ public class J2MC_Fun extends JavaPlugin implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         this.setFlight(event.getPlayer());
+    }
+    
+    
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+    	this.setFlight(event.getPlayer());
     }
 
     public void setFlight(Player player) {
